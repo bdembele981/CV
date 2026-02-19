c74f87ec466422394b9f0ef2b1736da846cda93a
bdembele981 <bdembele981@gmail.com>
2026-02-19 16:30:38 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+
+st.title("Profil Professionnel")
+
+st.write(
+"""
+Technicien supérieur en géomatique, je suis en mesure de fournir des cartes thématiques, topographiques .es SIG,
+la programmation et l’analyse géographique, je m’intéresse particulièrement
+à la gestion territoriale et aux applications environnementales.
+"""import streamlit as st
+
+st.title("Profil Professionnel")
+
+st.write(
+"""
+Technicien supérieur en géomatique, je combine rigueur technique,
+analyse spatiale et traitement de données pour produire des solutions
+cartographiques fiables et opérationnelles. Passionnée par les SIG,
+la programmation et l’analyse géographique, je m’intéresse particulièrement
+à la gestion territoriale et aux applications environnementales.
+"""
+)
+st.header("Compétences")
+
+st.markdown("""
+- Cartographie numérique et analyse spatiale (SIG : ArcGIS, QGIS)
+- Collecte et gestion de données géographiques (GPS, QField)
+- Analyse et traitement de données avec Python
+- Conception et gestion de bases de données spatiales
+- Réalisation de cartes thématiques et analytiques
+- Notions en *MMNT (Méthodes et Moyens Numériques de Travail)*
+- Bases en *Entrepreneuriat et gestion de projet*
+""")
+
+
+st.header("Projets académiques")                                                                                                                 st.markdown("""
+- *Cartographie SIG* : Collecte, traitement et visualisation de données géospatiales  
+- *Programmation Python* : Développement d’applications scientifiques et analyse de données  
+- *Dessin de plan* : Conception d’un plan architectural complet  
+- *Géographie appliquée* : Réalisation de cartes thématiques sous ArcGIS
+""")
+
+
+st.header("Formation")
+
+st.markdown("""
+- *BTS Géomatique* — Technicienne supérieure en Géomatique  
+- *MNT* : Méthodes et Moyens Numériques de Travail  
+- *SIG* : Systèmes d’Information Géographique  
+- *Entrepreneuriat* : Initiation à la création et gestion de projets
+""")                                                                                                                                                              
+st.header("Objectif professionnel")
+
+st.write(
+"""
+Mettre en compétences au service du port de ndayane pour réaliser les infrastructures routiére et ferroviaire du port de ndayane.
+""")
+
+st.sidebar.title("NIANGUIRI DEMBELE")
+st.sidebar.write("bdembele981@GAMIL.com.com")
+st.sidebar.write("Technicien supérieur en Géomatique")
+st.sidebar.write("  Pikine, Dakar - Sénégal")
